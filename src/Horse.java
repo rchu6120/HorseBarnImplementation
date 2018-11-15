53ae88a07782f82789f980be8ed7b7e95ab295d7
BT_1N3_01@BKTECH.LOCAL <BT_1N3_01@BKTECH.LOCAL>
2018-11-15 14:47:35 -0500

    Initial commit

---
+++ src/Horse.java
@@ -0,0 +1,19 @@
+public interface Horse {
+    private String Name;
+    private int Weight;
+    public Horse(String Name; int Weight) {
+        this.Name = Name;
+        this.Weight = Weight;
+    }
+    public String getName() {
+        return this.Name;
+    }
+    public int getWeight() {
+        return this.Weight;
+    }
+    public int findHorseSpace(String name) {
+        for (int i = 0; i < this.spaces.length; i ++) {
+
+        }
+    }
+}
---
+++ src/HorseRunner.java
@@ -0,0 +1,11 @@
+public class HorseRunner {
+    public static void main(String[] args) {
+        Horse horse1 = new Mustang("Secretariat", 12000);
+        Horse horse2 = new Mustang("Dusty Trail", 22000);
+        Horse horse3 = new Mustang("Silver", 12000);
+
+        Horse[] horses = new Horse[6];
+
+        horses
+    }
+}
